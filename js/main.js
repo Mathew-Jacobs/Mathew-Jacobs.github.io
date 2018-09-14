efe0ac2ad03b119c09aef80f239431ffe0dc1ded
Mathew-Jacobs <mathew.jacobs.exe@gmail.com>
2018-09-14 14:26:12 -0700

    javascript fix

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@
 
 jQuery(document).ready(function ($) {
 
-    console.log("A starter template created by Eleven Fifty Academy under MIT Licensing.");
 
     // Initialize wowjs 
     new WOW().init();
